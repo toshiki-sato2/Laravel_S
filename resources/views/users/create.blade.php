12e3ccc4259dac7a8206dd09ee247af9adc4dd36
toshiki.sato <toshiki-sato2@dac.co.jp>
2024-06-05 00:44:57 +0000

    プロフィール画像挿入画面作成

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -7,6 +7,27 @@
         <h3>新しいパスワードを設定をしない場合は空白のまま編集してください。</h3>
     </div>
 
+
+    <div class="flex justify-center">
+        <form method="POST" action="{{ route('users.update', \Auth::User()->id) }}" class="w-1/2">
+            @csrf
+            @method('PUT')
+
+                <div class="form-control my-4 ">
+                    <h2 class = "text-lg">プロフィール画像変更</h2>
+                </div>
+
+                <div class="form-control my-4">
+                    <label for="image" class="label">
+                        <span class="label-text">画像をファイルから選択</span>
+                    </label>
+                    <input type="file" name="image"  value = {{ \Auth::User()->name }} required>
+                </div>
+            <button type="submit" class="btn btn-primary btn-outline">編集完了</button>
+        </form>
+    </div>    
+    
+
     <div class="flex justify-center">
         <form method="POST" action="{{ route('users.update', \Auth::User()->id) }}" class="w-1/2">
             @csrf
